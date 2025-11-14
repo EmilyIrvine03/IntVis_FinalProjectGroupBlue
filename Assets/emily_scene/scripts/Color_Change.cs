21ecb41c2c737d23a5f4f754649c6866b8b01d5d
EmilyIrvine03 <e.irvine1@student.gsa.ac.uk>
2025-11-14 21:34:01 +0000

    14/11/25_Push(5)
    - created flicker script to make uterus material pulse

---
+++ Assets/emily_scene/scripts/Color_Change.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Color_Change : MonoBehaviour
+{
+
+    Color colorStart = Color.red;
+    Color colorEnd = Color.white;
+    float duration = 0.5f;
+    Renderer rend;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float lerp = Mathf.PingPong(Time.time, duration);
+        rend.material.color = Color.Lerp(colorStart, colorEnd, lerp);
+    }
+}
